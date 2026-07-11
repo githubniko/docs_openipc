3e1da0b954c0e616fca158cda5f979c1e5eb9c00
Egor <niko2on@users.noreply.github.com>
2026-07-11 14:58:43 -0400

    Fix deployed site URL for HTTP hosting

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://openipc.egorniko.ru",
+	site: "http://openipc.egorniko.ru",
 	base: "/",
 	integrations: [
 		starlight({
