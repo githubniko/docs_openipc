38d57ea325cee8c6b5e780551ba62bd7493deda7
Egor <niko2on@users.noreply.github.com>
2026-07-16 10:24:17 -0400

    config: restore production site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "http://openipc.egorniko.ru",
+	site: "https://docs.openipc.org",
 	base: "/",
 	integrations: [
 		starlight({
